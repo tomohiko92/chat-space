cb5c19649b961c892f34628a91bd484695a763b5
tomohiko92 <tomohiko.tokudo@di-v.co.jp>
2020-06-18 16:20:05 +0900

    コントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_06_17_102306) do
-
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
