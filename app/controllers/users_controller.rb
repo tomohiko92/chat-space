1e9cbcee311e76e8cefdd038af8973b5e3fd6d0e
tomohiko92 <tomohiko.tokudo@di-v.co.jp>
2020-06-27 17:35:41 +0900

    incremental search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def user_params
     params.require(:user).permit(:name, :email)
   end 
